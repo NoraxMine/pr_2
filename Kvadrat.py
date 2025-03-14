7ff0c3b65eaa39f906471844708163db51cad620
NoraxMine <NoraxMine@tutamail.com>
2025-03-14 08:53:12 +0300

    Finally

--- Kvadrat.py
+++ Kvadrat.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class Uravnenie:
-    def __init__(self,a,b,c,d):
+    def __init__(self, a, b, c, d):
         self.a = a
         self.b = b
         self.c = c
@@ -32,35 +32,13 @@ class Uravnenie:
         y = (self.a * (x ** 2)) + (self.b * x) + self.c
         return y
 
-    def paint(self):
-        xmin = -20.0
-        xmax = 20.0
-        count = 2000000
-
-        xlist = np.linspace(xmin, xmax, count)
-        ylist = [self.func(x) for x in xlist]
-        plt.plot(xlist, ylist, color = 'red')
-        plt.grid()
-        plt.show() 
-
 class Proizvod(Uravnenie):
-    def __init__(self, a, b, c):
-        super().__init__(a, b, c)
+    def __init__(self, a, b, c, d):
+        super().__init__(a, b, c, d)
 
     def func(self, x):
         y = (self.a * x * 2) + (self.b) 
         return y
-
-    def paint(self):
-        xmin = -20.0
-        xmax = 20.0
-        count = 2000000
-
-        xlist = np.linspace(xmin, xmax, count)
-        ylist = [self.func(x) for x in xlist]
-        plt.plot(xlist, ylist, color = 'purple')
-        plt.grid()
-        plt.show() 
 
     def __del__(self):                     
         print('!')
@@ -73,26 +51,28 @@ class Integral(Uravnenie):
         y = ((self.a * (x ** 3))/3) + ((self.b *(x ** 2))/2) + (self.c * x) + self.d
         return y
 
-    def paint(self):
-        xmin = -20.0
-        xmax = 20.0
-        count = 2000000
-
-        xlist = np.linspace(xmin, xmax, count)
-        ylist = [self.func(x) for x in xlist]
-        plt.plot(xlist, ylist, color = 'yellow')
-        plt.grid()
-        plt.show()
-
     def __del__(self):                     
         print('!')
 
 def lf():
-    u = Uravnenie(1,-2,1,0)
-    u.cor()
-    u.paint()
-    r = Proizvod(1,-2,1,0)
-    r.paint()
-    i = Integral(1,-2,1,0)
-    i.paint()
+    def plot_all(a, b, c, d):
+        x = np.linspace(-20, 20, 10_000)
+
+        u = Uravnenie(a, b, c, d)
+        p = Proizvod(a, b, c, d)
+        i = Integral(a, b, c, d)
+
+        u = u.func(x)  
+        p = p.func(x)  
+        i = i.func(x)  
+
+        plt.figure(figsize=(10, 6))  
+        plt.plot(x, u, color='red', label="Квадратичная функция")
+        plt.plot(x, p, color='purple', label="Производная")
+        plt.plot(x, i, color='yellow', label="Интеграл")
+        plt.grid()
+        plt.show()
+
+    plot_all(1, -2, 1, 3)
+
 lf()
